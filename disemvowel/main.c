7a138aa2b8dd29b4b3ce717327f9eab955ecc3f0
Robert-Beane <beane031@morris.umn.edu>
2020-09-17 11:04:42 -0500

    Fix memory leak for disemvowel

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -6,11 +6,15 @@
 int main(int argc, char *argv[]) {
   char *line;
   size_t size;
+  char *dummy; //just a dummy char to set as the actual thing so we dont have to remove the actual printed thing
   
   size = 100;
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    dummy = disemvowel(line);
+    printf("%s\n", dummy);
+    free(dummy); //frees the data from the dummy char we just set
   }
+  free(line); //frees the data from the line char
 }
