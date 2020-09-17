601c849bf155c8944b46f2cd1a6d51e56a8537cb
Robert-Beane <beane031@morris.umn.edu>
2020-09-17 10:15:31 -0500

    Add isAVowel function to check for vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
+  
   return (char*) "";
+}
+
+bool isAVowel(char c) { //Checks the given character if its a vowel or not
+  if (c == 'A' || c == 'a' || c == 'E' || c == 'e' || c == 'I' || c == 'i' || c == 'O' || c == 'o' || c == 'U' || c == 'u'){
+    return true;
+  } return false
 }
