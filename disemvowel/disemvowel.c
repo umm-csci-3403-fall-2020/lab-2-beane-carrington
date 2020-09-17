b97d066efb0657ee2e13532ec05ec169323f3102
Robert-Beane <beane031@morris.umn.edu>
2020-09-17 10:37:18 -0500

    ADd working disemvowel function. Memory leaks still present.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -6,14 +6,25 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  
-  return (char*) "";
+  int c = consonantsSize(str);
+  char *final = (char*) calloc(c+1, sizeof(char)); //sets up the memory
+  int length = strlen(str);
+
+  int k = 0; 
+  for (int i = 0; i < length; i++){
+    if (!isAVowel(str[i])){
+	final[k] = str[i];
+	k++;
+      }
+    }
+  final[k] = '\0'; //as stated in the lab documentation, need this to allocate final character
+  return final;
 }
 
 bool isAVowel(char c) { //Checks the given character if its a vowel or not
   if (c == 'A' || c == 'a' || c == 'E' || c == 'e' || c == 'I' || c == 'i' || c == 'O' || c == 'o' || c == 'U' || c == 'u'){
     return true;
-  } return false
+  } return false;
 }
 
 int consonantsSize (char *str) { //this function counts the number of consonants so we can have the exact size of memory.
--- disemvowel/disemvowel.h
+++ disemvowel/disemvowel.h
@@ -2,5 +2,7 @@
 #define DISEMVOWEL_H_GUARD
 
 char *disemvowel(char *str);
+int consonantsSize(char *str);
+bool isAVowel(char c);
 
 #endif
